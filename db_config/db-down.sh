5d02f3a477912f709ab430789eb1e6921bad4d34
twallick <twallick@vivsoft.io>
2021-09-21 23:23:12 +0200

    added db-down.sql and .sh scripts

--- db_config/db-down.sh
+++ db_config/db-down.sh
@@ -4,6 +4,6 @@ POSTGRES_DB_OWNER=isgogolgo13
 POSTGRES_DB=enginedb
 
 psql -f db_config/db-down.sql --host=localhost --username=${POSTGGRES_DB_OWNER} --dbname=${POSTGRES_DB}
-dropdb ${POSTGRES_DB}
+dropdb  ${POSTGRES_DB}
 dropuser ${POSTGRES_DB_OWNER}
 
