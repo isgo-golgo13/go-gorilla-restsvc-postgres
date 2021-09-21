0f26041681a0a5bff242a699ab77fddc7a01da23
twallick <twallick@vivsoft.io>
2021-09-21 22:59:12 +0200

    added db-down.sql and .sh scripts

---
+++ db_config/db-down.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+POSTGRES_DB_OWNER=isgogolgo13
+POSTGRES_DB=enginedb
+
+psql -f db_config/db-down.sql --host=localhost --username=${POSTGGRES_DB_OWNER} --dbname=${POSTGRES_DB}
+dropdb ${POSTGRES_DB}
+dropuser ${POSTGRES_DB_OWNER}
+
---
+++ db_config/db-up.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+POSTGRES_DB_OWNER=isgogolgo13
+POSTGRES_DB=enginedb
+
+createuser --createdb --createrole --login ${POSTGRES_DB_OWNER}  -e 
+#createdb ${POSTGRES_DB} --owner=${POSTGRES_DB_OWNER} # Superuse can only do this
+createdb ${POSTGRES_DB}
+psql -f db_config/db-up.sql --host=localhost --username=${POSTGGRES_DB_OWNER} --dbname=${POSTGRES_DB}
